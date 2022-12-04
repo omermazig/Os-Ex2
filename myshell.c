da3693ce5687dc2446be6f9ca21d4229f5ad8a9a
student <omermazig@mail.tau.ac.il>
2022-12-04 12:36:05 +0200

    Move function to a new `myshell.c` file and added `myshell.h`/

---
+++ myshell.c
@@ -0,0 +1,16 @@
+#define _GNU_SOURCE
+
+// arglist - a list of char* arguments (words) provided by the user
+// it contains count+1 items, where the last item (arglist[count]) and *only* the last is NULL
+// RETURNS - 1 if should continue, 0 otherwise
+int process_arglist(int count, char** arglist){
+    return 0;
+}
+
+// prepare and finalize calls for initialization and destruction of anything required
+int prepare(void){
+    return 0;
+}
+int finalize(void){
+    return 0;
+}
