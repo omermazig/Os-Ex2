db4e6c041edd46d9dbbce1e8f0cf379ab2164475
student <omermazig@mail.tau.ac.il>
2022-12-04 10:10:44 +0200

    Empty commit to see it running

--- shell.c
+++ shell.c
@@ -3,15 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 
 // arglist - a list of char* arguments (words) provided by the user
 // it contains count+1 items, where the last item (arglist[count]) and *only* the last is NULL
 // RETURNS - 1 if should continue, 0 otherwise
-int process_arglist(int count, char** arglist);
+int process_arglist(int count, char** arglist){
+    return 0;
+}
 
 // prepare and finalize calls for initialization and destruction of anything required
-int prepare(void);
-int finalize(void);
+int prepare(void){
+    return 0;
+}
+int finalize(void){
+    return 0;
+}
 
 int main(void)
 {
