25b32c1f928e99c722205a6bc2b891c58a9b3956
omermazig <omermazig@gmail.com>
2022-12-05 12:34:19 +0200

    Submittion

--- shell.c
+++ shell.c
@@ -15,53 +15,53 @@ int finalize(void);
 
 int main(void)
 {
-    if (prepare() != 0)
-        exit(1);
+	if (prepare() != 0)
+		exit(1);
+	
+	while (1)
+	{
+		char** arglist = NULL;
+		char* line = NULL;
+		size_t size;
+		int count = 0;
 
-    while (1)
-    {
-        char** arglist = NULL;
-        char* line = NULL;
-        size_t size;
-        int count = 0;
+		if (getline(&line, &size, stdin) == -1) {
+			free(line);
+			break;
+		}
+    
+		arglist = (char**) malloc(sizeof(char*));
+		if (arglist == NULL) {
+			printf("malloc failed: %s\n", strerror(errno));
+			exit(1);
+		}
+		arglist[0] = strtok(line, " \t\n");
+    
+		while (arglist[count] != NULL) {
+			++count;
+			arglist = (char**) realloc(arglist, sizeof(char*) * (count + 1));
+			if (arglist == NULL) {
+				printf("realloc failed: %s\n", strerror(errno));
+				exit(1);
+			}
+      
+			arglist[count] = strtok(NULL, " \t\n");
+		}
+    
+		if (count != 0) {
+			if (!process_arglist(count, arglist)) {
+				free(line);
+				free(arglist);
+				break;
+			}
+		}
+    
+		free(line);
+		free(arglist);
+	}
+	
+	if (finalize() != 0)
+		exit(1);
 
-        if (getline(&line, &size, stdin) == -1) {
-            free(line);
-            break;
-        }
-
-        arglist = (char**) malloc(sizeof(char*));
-        if (arglist == NULL) {
-            printf("malloc failed: %s\n", strerror(errno));
-            exit(1);
-        }
-        arglist[0] = strtok(line, " \t\n");
-
-        while (arglist[count] != NULL) {
-            ++count;
-            arglist = (char**) realloc(arglist, sizeof(char*) * (count + 1));
-            if (arglist == NULL) {
-                printf("realloc failed: %s\n", strerror(errno));
-                exit(1);
-            }
-
-            arglist[count] = strtok(NULL, " \t\n");
-        }
-
-        if (count != 0) {
-            if (!process_arglist(count, arglist)) {
-                free(line);
-                free(arglist);
-                break;
-            }
-        }
-
-        free(line);
-        free(arglist);
-    }
-
-    if (finalize() != 0)
-        exit(1);
-
-    return 0;
+	return 0;
 }
